5091b3ca0691bd97ee9c528b0d45399b9bd54326
Pranav Varpe <varpepranav8@gmail.com>
2025-01-29 19:18:11 +0530

    Time: 1 ms, Memory: 46.6 MB - LeetHub

---
+++ 17127-776-136-single-number/17127-776-136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n =nums.length;
+        int value = 0;
+        for(int i=0;i<n;i++){
+            value = value^nums[i];
+        }
+
+        return value;
+    }
+}
