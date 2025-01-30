ac13d3b06369cb55f6c7f70588d09a8a603597bc
Pranav Varpe <varpepranav8@gmail.com>
2025-01-30 19:14:30 +0530

    Time: 4 ms, Memory: 45.7 MB - LeetHub

---
+++ 8465-468-680-valid-palindrome-ii/8465-468-680-valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int a = 0;
+        int b = s.length()-1;
+        while(a<=b){
+            if(s.charAt(a) == s.charAt(b)){
+                a++;
+                b--;
+            }
+            else return isPalindrome(s,a+1,b)||isPalindrome(s,a,b-1);
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int a, int b){
+        while(a<=b){
+            if(s.charAt(a)==s.charAt(b)){
+                a++;
+                b--;
+            }
+            else return false;
+        }
+        return true;            
+    }
+    
+}
