d0986da0ea848d3ac7c78568a0ab9a4b241a5ff8
Pranav Varpe <varpepranav8@gmail.com>
2025-02-12 21:17:08 +0530

    Time: 5 ms, Memory: 45.6 MB - LeetHub

---
+++ 8193-715-137-single-number-ii/8193-715-137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>hm = new HashMap<>();
+        for (int num:nums) {
+                hm.put(num, hm.getOrDefault(num, 0)+1);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(hm.get(nums[i])==1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
