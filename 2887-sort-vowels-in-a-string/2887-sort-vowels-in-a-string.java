65481574c47339e1d9c27ad41f6d9699be6bfa37
Pranav Varpe <varpepranav8@gmail.com>
2025-09-11 19:11:03 +0530

    Time: 61 ms (30.65%), Space: 48.7 MB (22.72%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> list = new ArrayList<>();
+        Set<Character> set = new HashSet<>(Arrays.asList(
+                'a', 'e', 'i', 'o', 'u',
+                'A', 'E', 'I', 'O', 'U'
+        ));
+        for(char c : s.toCharArray()){
+            if(set.contains(c)){
+                list.add(c);
+            }
+        }
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+         for(char c : s.toCharArray()){
+           if(set.contains(c)){
+            sb.append(list.get(index++));
+           } else{
+            sb.append(c);
+           }
+        }
+        return sb.toString();
+    }
+ 
+}
