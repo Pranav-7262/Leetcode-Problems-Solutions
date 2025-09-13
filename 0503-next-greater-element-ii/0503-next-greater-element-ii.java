93c071ce13cd4dc8379d8d1b0dc69986073c419d
Pranav Varpe <varpepranav8@gmail.com>
2025-09-13 17:58:44 +0530

    Time: 10 ms (83.86%), Space: 46.5 MB (38.2%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int n = nums.length;
+        int []arr = new int[n];
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = -1;
+        } 
+        for(int i=0;i<n*2;i++) {
+          
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i%n]) {
+                arr[stack.pop()] =  nums[i%n];
+            }
+            if(i < n) {
+                stack.push(i);
+            }
+        }
+        return arr;
+    }
+}
