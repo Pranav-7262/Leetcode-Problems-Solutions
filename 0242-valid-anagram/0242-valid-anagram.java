d38aa360365542cabc3afe92a46888a23d5b38a1
Pranav Varpe <varpepranav8@gmail.com>
2025-10-13 20:35:25 +0530

    Time: 3 ms (90.42%), Space: 44.8 MB (34.74%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
