40e900df4c0f7ecb718cb5ddd9fd00fb89acaec0
Pranav Varpe <varpepranav8@gmail.com>
2025-10-31 18:43:40 +0530

    Time: 1 ms (100%), Space: 45.6 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-         int [] arr = new int[2];
-        int i=0;
-       for(int num:map.keySet()){
-        if(map.get(num)==2){
-            arr[i++] = num;
-        }
-      
+      int n = nums.length;
+      int []res = new int[2];
+      int c=0;
+      for(int i=0;i<n;i++) {
+         for(int j=i+1;j<n;j++) {
+           if(nums[i] == nums[j]) {
+             res[c++] = nums[i];
+             break;
+           }
        }
-       return arr;
+      }
+      return res;
     }
 }
