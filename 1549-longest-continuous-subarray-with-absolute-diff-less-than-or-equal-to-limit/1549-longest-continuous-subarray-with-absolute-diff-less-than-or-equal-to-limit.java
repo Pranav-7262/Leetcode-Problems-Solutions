24b94c1b1b966320fe6a389d4a2608926e501aaa
Pranav Varpe <varpepranav8@gmail.com>
2025-11-10 19:31:20 +0530

    Time: 134 ms (10.87%), Space: 91.4 MB (6.09%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,31 +1,20 @@
 class Solution {
     public int longestSubarray(int[] arr, int x) {
          int n = arr.length;
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        int s = 0 , e = 0 , maxlen = 0 ,bestS = 0;
-        
-        while(e < n) {
-            map.put(arr[e] , map.getOrDefault(arr[e],0)+1);
-            
-            while(map.lastKey() - map.firstKey() > x){
-                 map.put(arr[s] , map.getOrDefault(arr[s],0)-1);
-                 if(map.get(arr[s]) == 0){
-                     map.remove(arr[s]);
-                 }
-                 s++;
-            }
-            if(e-s+1 > maxlen){
-                maxlen = e-s+1;
-                bestS = s;
-            }
-            e++;
-        }
-         ArrayList<Integer> list = new ArrayList<>();
+		TreeMap<Integer ,Integer> map = new TreeMap<>();
+		int start = 0 , ans =0;
 
-        for (int i = 0; i < maxlen; i++) {
-            list.add(arr[bestS + i]);
-        }
-        return list.size();
+		for(int end=0;end<n;end++) {
+			map.put(arr[end] , map.getOrDefault(arr[end] ,0)+1);
+
+			while(map.lastKey() - map.firstKey() > x) {
+				map.put(arr[start] , map.get(arr[start])-1);
+				if(map.get(arr[start]) == 0) map.remove(arr[start]);
+				start++;
+			}
+			ans = Math.max(ans,end-start+1);
+		}
+		return ans;
     
     }
 }
