f65903f36fcf853f130b92db2ed10f50e8f469f8
Pranav Varpe <varpepranav8@gmail.com>
2025-11-22 16:11:13 +0530

    Time: 31 ms (97.76%), Space: 52.8 MB (95.54%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    const greet = "Hello World";
+    return function() {
+        return greet;
+    }
+};
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
