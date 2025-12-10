a1ec32fa3dbaad73920598922b4d4bf876f25d1b
Pranav Varpe <varpepranav8@gmail.com>
2025-12-10 18:28:27 +0530

    Time: 0 ms (100%), Space: 42.3 MB (51.11%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,12 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 0) return false;
-        if(n == 1){
-            return true;
-        }
-        if(n%4 != 0){
+        if (n <= 0 || (n & (n - 1)) != 0)
             return false;
-        }
-        return isPowerOfFour(n/4);
+        return (n & 0xAAAAAAAA) == 0; 
     }
 }
