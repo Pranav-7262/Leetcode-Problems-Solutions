90f56cfe1e98f8bc9289cafba47062391f3ba817
Pranav Varpe <varpepranav8@gmail.com>
2025-12-10 18:20:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger num1 = new BigInteger(a, 2);
+        BigInteger num2 = new BigInteger(b, 2);
+        BigInteger ans = num1.add(num2);
+        return ans.toString(2);
+    }
+}
