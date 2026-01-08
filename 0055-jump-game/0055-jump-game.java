33adc5cbde53492bab9a06b368266c9756d268dd
Pranav Varpe <varpepranav8@gmail.com>
2026-01-08 09:25:13 +0530

    Time: 2 ms (84.47%), Space: 48 MB (43.67%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxi = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++) {
+            if(i > maxi){
+                return false;
+            }
+            maxi= Math.max(maxi ,nums[i]+i);
+        }
+        return true;
+    }
+}
