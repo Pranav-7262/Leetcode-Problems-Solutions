640ac86f3363adcc9668ec997cee7002768d4796
Pranav Varpe <varpepranav8@gmail.com>
2026-02-19 09:30:11 +0530

    Time: 10 ms (87.5%), Space: 46.3 MB (72.72%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int n = s.length();
+        int prevGroup = 0;
+        int currGroup = 1;
+        int result = 0;
+        for(int i=1;i<n;i++) {
+            if(s.charAt(i) == s.charAt(i-1)) {
+                currGroup++;
+            }else {
+                result += Math.min(currGroup , prevGroup);
+                prevGroup = currGroup;
+                currGroup = 1;
+            }
+        }
+         result += Math.min(currGroup , prevGroup);
+         return result;
+    }
+}
